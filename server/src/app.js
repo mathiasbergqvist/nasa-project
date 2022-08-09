3e40f8fe11e77a833162313ef5b1cf110ab664ef
Mathias <mathiasbergqvist88@gmail.com>
2022-08-09 16:45:34 +0200

    Serving react front end in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const planetsRouter = require("./routes/planets/planets.router");
 const cors = require("cors");
 
@@ -11,6 +12,14 @@ app.use(
   })
 );
 app.use(express.json());
+// Serve client
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+
+// Route to index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
