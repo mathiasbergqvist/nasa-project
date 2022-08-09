a0f9cbf6a6a2b43efe34d2377a8037a51835671f
Mathias <mathiasbergqvist88@gmail.com>
2022-08-09 16:57:30 +0200

    Logging requests with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -2,19 +2,25 @@ const express = require("express");
 const path = require("path");
 const planetsRouter = require("./routes/planets/planets.router");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
+// CORS policy
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     optionsSuccessStatus: 200,
   })
 );
+// Setup logging with Morgan
+app.use(morgan("combined"));
+
 app.use(express.json());
 // Serve client
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+// Internal routes
 app.use(planetsRouter);
 
 // Route to index.html
