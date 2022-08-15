8d3d9545e1eabe569c5ef634f986d139a4f915ce
Mathias <mathiasbergqvist88@gmail.com>
2022-08-15 09:56:19 +0200

    Testing API endpoints with supertest: POST

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,7 +11,32 @@ describe("GET /launches", () => {
 });
 
 describe("POST /launch", () => {
-  it("should respond with 200 success", () => {});
+  const completeLaunchData = {
+    mission: "USS Enterprice",
+    rocket: "NCC 1701-D",
+    target: "Kepler 186-f",
+    launchDate: "Januari 4, 2023",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "USS Enterprice",
+    rocket: "NCC 1701-D",
+    target: "Kepler 186-f",
+  };
+
+  it("should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
   it("should catch missing required properties", () => {});
   it("should catch invalid dates", () => {});
 });
