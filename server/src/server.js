a919bfd4e8b669516440f4ca30ced4757d463bb7
Mathias <mathiasbergqvist88@gmail.com>
2022-10-28 16:02:42 +0200

    Connect to mongodb database

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,29 @@
 const http = require("http");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("🔌 MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error(`Error connecting to MongoDB: ${error}`);
+});
+
 const startServer = async () => {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
