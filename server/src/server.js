4c021e10b56862ca5b411a2347e98cf3e44d6674
Mathias <mathiasbergqvist88@gmail.com>
2023-02-06 19:46:58 +0100

    Mapping spacex data to our api

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 const { mongoConnect } = require("./services/mongo");
+const { loadLaunchData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
@@ -10,6 +11,7 @@ const server = http.createServer(app);
 const startServer = async () => {
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`🚀 Listening on port ${PORT}...`);
