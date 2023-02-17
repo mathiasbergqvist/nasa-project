d7daa2651780345e4087efe3407b1e7c6067d611
Mathias <mathiasbergqvist88@gmail.com>
2023-02-17 16:14:14 +0100

    Localhost server

--- server/lighthouserc.js
+++ server/lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     upload: {
       target: "lhci",
-      serverBaseUrl: "http://localhost.svtplay.se:9001/",
+      serverBaseUrl: "http://localhost:9001/",
       token: "d2322fd4-e852-4717-a4ac-bf5b21fa8c58", // could also use LHCI_TOKEN variable instead
     },
   },
