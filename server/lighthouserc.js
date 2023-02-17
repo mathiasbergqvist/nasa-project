a752e110df2396fb710ab2766872692e9cce8822
Mathias <mathiasbergqvist88@gmail.com>
2023-02-17 16:07:19 +0100

    Update config

--- server/lighthouserc.js
+++ server/lighthouserc.js
@@ -1,7 +1,9 @@
 module.exports = {
   ci: {
     upload: {
-      target: "temporary-public-storage",
+      target: "lhci",
+      serverBaseUrl: "https://your-lhci-server-url.example.com",
+      token: "d2322fd4-e852-4717-a4ac-bf5b21fa8c58", // could also use LHCI_TOKEN variable instead
     },
   },
 };
